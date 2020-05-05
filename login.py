e5a66873a51180aabe726f760f00dee28bf57664
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2020-05-05 16:24:12 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,13 @@
+
+
+with open('auth.txt', 'r') as users:
+    for line in users:
+        username, password = line.split(',')
+        print("Welcome, before you get started please enter your username")
+        supplied_username = input()
+        if username == supplied_username:
+            print("Please enter you password")
+            supplied_password = input()
+            if password == supplied_password:
+                print("Login Successful, the game will start now ")
+                exec(open("game.py").read())
